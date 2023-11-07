76df3f21fe7897a6db0bf59ab2aeb7783188c210
kevin <rwnkevinchauhan@gmail.com>
2023-11-07 22:10:33 +0530

    add auth controller

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
+import 'reflect-metadata'
 import express from 'express'
 import logger from './config/logger.js'
-import errorHandler from './middleware/errorHandler.js'
+import errorHandler from './middlewares/errorHandler.js'
+import router from './routes/auth.js'
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('hello...')
-})
+// auth routes
+app.use('/auth', router)
 
+// error handler
 app.use(errorHandler)
 
 export default app
---
+++ src/controllers/AuthController.js
@@ -0,0 +1,5 @@
+export class AuthController {
+    register(req, res) {
+        res.json({ msg: 'registering...' })
+    }
+}
