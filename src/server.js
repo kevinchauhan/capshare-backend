1dcccdeaa478c7f6ce30fbcfccd4cac535dc8af5
kevin <rwnkevinchauhan@gmail.com>
2023-11-07 18:20:00 +0530

    logger setup

--- src/server.js
+++ src/server.js
@@ -1,16 +1,18 @@
 import app from './app.js'
 import { Config } from './config/index.js'
+import logger from './config/logger.js'
 
 const startServer = async () => {
     const PORT = Config.PORT
     try {
         app.listen(PORT, () => {
-            // eslint-disable-next-line
-            console.log('listening on PORT', PORT)
+            logger.info(`listening on PORT ${PORT}`)
         })
     } catch (error) {
-        console.log(error)
-        process.exit(1)
+        logger.error(error.message)
+        setTimeout(() => {
+            process.exit(1)
+        }, 1000)
     }
 }
 startServer()
